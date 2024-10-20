4ba0d41db65ec95f501930a73594199df814aaa5
thiagocosta70 <thiagoemanuel2504@gmail.com>
2024-10-20 12:14:57 -0300

    Adicionar arquivos de projeto.

---
+++ LoginTeste/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginTeste.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatorio informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
